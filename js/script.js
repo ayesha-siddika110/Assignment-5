08f6bd59a78c3f33f4ded3c3db660f388cbc6682
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-09-24 11:33:24 +0600

    project done

--- js/script.js
+++ js/script.js
@@ -8,20 +8,22 @@ function callDonateValue(id) {
     const getDonateValue = (document.getElementById(id))
     return getDonateValue;
 }
-// function callAvailableBalance(id){
-//     const getAvailableBalance = document.getElementById('available-balance').innerText;
-//     return getAvailableBalance
 
-// }
-
+// section 1
 
 let donateAmount = 0;
-function donatNow() {
+const donateBtn = document.getElementById('donate-btn');
+donateBtn.addEventListener('click',function () {
+    // event.preventDefault();
+    // console.log('connecting');
+    
     const getInputField = callInputValue('input-field')
     const getDonateValue = callDonateValue('donate-amount')
 
 
     // available balance
+
+
 
 
     const getAvailableBalance = parseFloat(document.getElementById('available-balance').innerText);
@@ -35,16 +37,24 @@ function donatNow() {
         const newBalance = getAvailableBalance - getInputField;
         document.getElementById('available-balance').innerText = newBalance;
 
+        // const btn = document.getElementById('my_modal_1');
+        // btn.classList.remove('hidden');
+        alert("Succecfully Donate money")
+
+
     }
     else if (getInputField <= 0) {
-        alert("input a valid amount");
+        // const btn = document.getElementById('my_modal_1');
+        // btn.classList.add('hidden');
+        alert("please enter a valid amount");
+        
     }
     else {
-        alert("invalid amount");
+        alert("please enter a valid amount");
     }
 
     const history = document.getElementById('history-tab');
-    history.addEventListener('click', function () {
+    history.addEventListener('click', function donatNowS2() {
         const getHistoryArea = document.getElementById('history-area');      
 
         getHistoryArea.innerHTML += `
@@ -61,11 +71,13 @@ function donatNow() {
 
 
 
-}
+})
 
+// section 2
 
 let donateAmounts2 = 0;
-function donatNowS2() {
+const donateBtnS2 = document.getElementById('donate-btn-s2')
+donateBtnS2.addEventListener('click',function () {
     const getInputField = callInputValue('input-field-s2')
     const getDonateValue = callDonateValue('donate-amount-s2')
 
@@ -73,6 +85,7 @@ function donatNowS2() {
 
     const getAvailableBalance = parseFloat(document.getElementById('available-balance').innerText);
 
+    
     if (getInputField <= getAvailableBalance && getInputField > 0) {
         donateAmounts2 = donateAmounts2 + getInputField;
 
@@ -81,13 +94,24 @@ function donatNowS2() {
 
         const newBalance = getAvailableBalance - getInputField;
         document.getElementById('available-balance').innerText = newBalance;
+        alert("Succecfully Donate Money")
+
+        // const btn = document.getElementById('my_modal_5');
+
+
+        // btn.classList.remove('hidden');
+       
+      
+        
+
 
     }
     else if (getInputField <= 0) {
-        alert("Please enter a valid amount")
+        alert("please enter a valid amountdff");
+        
     }
-    else{
-        alert("Please enter a valid amount")
+    else {
+        alert("please enter a valid amount");
     }
     const history = document.getElementById('history-tab');
     history.addEventListener('click', function () {
@@ -106,11 +130,12 @@ function donatNowS2() {
     })
     
     
-}
+}) 
 
-// section 3 
+// // section 3 
 let donateAmountS3 = 0;
-function donatNowS3() {
+const donateBtnS3 = document.getElementById('donate-btn-s3')
+donateBtnS3.addEventListener('click',function donatNowS2() {
     const getInputField = callInputValue('input-field-s3')
     const getDonateValue = callDonateValue('donate-amount-s3')
     
@@ -127,6 +152,7 @@ function donatNowS3() {
         
         const newBalance = getAvailableBalance - getInputField;
         document.getElementById('available-balance').innerText = newBalance;
+        alert("Succesfully Donate Money")
 
     }
     else if (getInputField <= 0) {
@@ -138,6 +164,7 @@ function donatNowS3() {
     
     const history = document.getElementById('history-tab');
     history.addEventListener('click', function () {
+        
         const getHistoryArea = document.getElementById('history-area');      
     
         getHistoryArea.innerHTML += `
@@ -153,15 +180,7 @@ function donatNowS3() {
     })
 
 
-}
-
-// // available balance
-
-
-// const getAvailableBalance = parseFloat(document.getElementById('available-balance').innerText)
-// console.log(getAvailableBalance);
-
-
+})
 
 
 
--- js/showhmenu.js
+++ js/showhmenu.js
@@ -6,6 +6,17 @@ historyTab.addEventListener('click', function(){
     
     document.getElementById('history-area').classList.remove('hidden');
     document.getElementById('donation-area').classList.add('hidden');
+    document.getElementById('history-tab').classList.remove('bg-white');
+    document.getElementById('donation-tab').classList.add('bg-white');
+    document.getElementById('history-tab').classList.add('bg-[#B4F461]');
+    document.getElementById('history-tab').classList.add('hover:bg-[#B4F461]');
+    document.getElementById('history-tab').classList.add('border-[#B4F461]');
+    document.getElementById('donation-tab').classList.remove('border-[#B4F461]');
+    document.getElementById('donation-tab').classList.add('border-gray-300');
+    document.getElementById('history-tab').classList.add('hover:border-[#B4F461]');
+    
+    
+
 })
 const donationTab = document.getElementById('donation-tab');
 donationTab.addEventListener('click', function(){
@@ -13,4 +24,15 @@ donationTab.addEventListener('click', function(){
     
     document.getElementById('history-area').classList.add('hidden');
     document.getElementById('donation-area').classList.remove('hidden');
+    document.getElementById('donation-tab').classList.add('bg-[#B4F461]');
+    document.getElementById('history-tab').classList.remove('bg-[#B4F461]');
+    document.getElementById('donation-tab').classList.remove('bg-white');
+    document.getElementById('donation-tab').classList.add('bg-[#B4F461]');
+    document.getElementById('history-tab').classList.remove('border-[#B4F461]');
+    document.getElementById('donation-tab').classList.add('hover:bg-[#B4F461]');
+    document.getElementById('donation-tab').classList.add('hover:border-[#B4F461]');
+    document.getElementById('donation-tab').classList.add('border-[#B4F461]');
+    document.getElementById('history-tab').classList.add('bg-white');
+
+   
 })
